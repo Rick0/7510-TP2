63abe0229f24ba3289f544ab883bba013eafca93
Lucas Tarcetti <shamankingha@gmail.com>
2013-10-30 22:15:48 -0300

    Faltaba la inicializacion de la lista de resultados. La agrego y agrego en PruebaSumasVarias una prueba que parece funcionar

--- TP2-JUnit/src/main/java/Grupo8/PruebaSumasVarias.java
+++ TP2-JUnit/src/main/java/Grupo8/PruebaSumasVarias.java
@@ -1,5 +1,8 @@
 package Grupo8;
 
+import java.util.List;
+import java.util.Iterator;
+
 public class PruebaSumasVarias {	
 	TestSuite conjunto = new TestSuite();
 	
@@ -7,11 +10,25 @@ public class PruebaSumasVarias {
 		TestCase test1 = new TestCase("UnoYUno");
 		test1.setUp(2,2);		
 		TestCase test2 = new TestCase("DosYDos");
-		test2.setUp(2,2);		
+		test2.setUp(2,1);		
 		conjunto.addTest(test1);
 		conjunto.addTest(test2);
 		TestResult resultados = new TestResult();
-		conjunto.run(resultados);		
+		conjunto.run(resultados);
+		System.out.println("Ok");
+		imprimirLista(resultados.getListPassed());
+		System.out.println("Fallados");
+		imprimirLista(resultados.getListFailure());
+				
+	}
+	
+	private void imprimirLista(List<Test> lista){
+		Iterator it = lista.iterator();
+		while (it.hasNext()){
+			TestCase t = (TestCase)it.next();
+			System.out.println(t.getNombre());
+		}
+			
 	}
 }
 
--- TP2-JUnit/src/main/java/Grupo8/TestResult.java
+++ TP2-JUnit/src/main/java/Grupo8/TestResult.java
@@ -1,6 +1,7 @@
 package Grupo8;
 
 import java.util.List;
+import java.util.ArrayList;
 
 public class TestResult {
 	List<Test> testsPassed;
@@ -9,7 +10,9 @@ public class TestResult {
 	
 	
 	public TestResult(){
-		
+		testsPassed = new ArrayList<Test>();
+		testsFailed = new ArrayList<Test>();
+		testsError = new ArrayList<Test>();
 	}	
 	
 	public void addFailure(Test test){
