07682a6d85e2387c751083a7f9c2cbe82aa6955c
Rick0 <ycg.zhang@gmail.com>
2013-10-31 10:39:16 -0300

    "assertion" deprecated en "test"

--- TP2-JUnit/src/main/java/Grupo8/Test.java
+++ TP2-JUnit/src/main/java/Grupo8/Test.java
@@ -2,12 +2,6 @@ package Grupo8;
 
 public abstract class Test {
 	
-	protected Assertions evaluator;
-	
-	
-	Test () {
-		evaluator = new Assertions();
-	}
-	
 	public void runTest(TestResult result) {};
+	
 }
