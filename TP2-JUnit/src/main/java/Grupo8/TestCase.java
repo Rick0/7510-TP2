cdcb3c8e7d19ac39aea2bb1dab3a44511491c332
daniel <incorporado@gmail.com>
2013-10-30 23:01:30 -0300

    https://github.com/Rick0/7510-TP2.git

--- TP2-JUnit/src/main/java/Grupo8/TestCase.java
+++ TP2-JUnit/src/main/java/Grupo8/TestCase.java
@@ -4,10 +4,12 @@ public class TestCase extends Test {
 	private String nombre;	
 	private Object valorATestear1;
 	private Object valorATestear2;
+	private boolean valoresSeteados;
 	//private TestResult unTestResult;
 	
 	TestCase (String nombre){
 		this.setNombre(nombre);
+		this.valoresSeteados = false;
 		//this.unTestResult = new TestResult();
 	}
 	
@@ -15,22 +17,28 @@ public class TestCase extends Test {
 	public void setUp(Object valorATestear1, Object valorATestear2){
 		this.valorATestear1 = valorATestear1;
 		this.valorATestear2 = valorATestear2;
+		this.valoresSeteados = true;
 	}
 	
 	//Debe ser implementado por el cliente. Aquí está el assertTrue.
 	public void runTest(TestResult result)  {
-		try{
-			this.evaluador.testear(this.valorATestear1,this.valorATestear2);
-		}		
-		catch(OkException e){
-			result.addPassed(this);
+		if (this.valoresSeteados){
+			try{
+				this.evaluador.testear(this.valorATestear1,this.valorATestear2);
+			}		
+			catch(OkException e){
+				result.addPassed(this);
+			}
+			catch(FailureException e){
+				result.addFailure(this);
+			}		
+			catch(Throwable e){
+				result.addError(this);
+			}
 		}
-		catch(FailureException e){
-			result.addFailure(this);
-		}		
-		catch(Throwable e){
+		else {
 			result.addError(this);
-		}
+		}		
 	}	
 	
 
