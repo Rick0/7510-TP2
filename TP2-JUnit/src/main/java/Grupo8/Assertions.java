56806b32a7c069f3e2e3896f8279613b8d2b38b4
daniel <incorporado@gmail.com>
2013-10-31 00:09:51 -0300

    https://github.com/Rick0/7510-TP2.git

--- TP2-JUnit/src/main/java/Grupo8/Assertions.java
+++ TP2-JUnit/src/main/java/Grupo8/Assertions.java
@@ -1,28 +1,14 @@
 package Grupo8;
 
 public class Assertions {
-	public boolean assertTrue(){return true;}
-	//tiene que arrojar una Excepcion
-	//public void assertEquals(int a, int b){
-		//NullPointerException e = new NullPointerException();
-		//ExceptionInInitializerError f = new ExceptionInInitializerError();
-// Cree un par de excepciones. Fijate si van. Si es asi, dejalas.		
-//		OkException e = new OkException();
-//		FailureException f = new 	();
-//		ErrorException er = new ErrorException();
-		//if (a!=b) throw e;
-		//else throw f;
-	//}
-	
 	public void testear (Object valorATestear1, Object valorATestear2) throws Throwable {
-		//if (valorATestear1 != valorATestear2) {
 		if (!valorATestear1.equals(valorATestear2)) {
 			throw new FailureException();			
 		}
 		else {
 			throw new OkException();
 		}
-	}
+	}	
 	
 /*
 	public void assertEquals(String a, String b){
---
+++ TP2-JUnit/src/test/java/Grupo8/Testing.java
@@ -0,0 +1,68 @@
+package Grupo8;
+
+import static org.junit.Assert.*;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Testing {
+
+	@Test
+	public void test1() {
+		Assertions tester = new Assertions();
+		int a = 0;
+		int b = 0;
+		try {
+			tester.testear(a, b);
+		}
+		catch (Throwable e){
+			assertTrue(e instanceof OkException);
+		}
+	}
+	
+	@Test
+	public void test2() {
+		Assertions tester = new Assertions();
+		int a = 0;
+		int b = 1;
+		try {
+			tester.testear(a, b);
+		}
+		catch (Throwable e){
+			assertFalse(e instanceof OkException);
+			assertTrue(e instanceof FailureException);
+		}
+	}
+	
+	@Test
+	public void test3() {		
+		TestCase test = new TestCase("test");
+		TestResult result = new TestResult ();
+		Assert.assertTrue(result.getListError().size() == 0);
+		test.runTest(result);
+		Assert.assertTrue(result.getListError().size() == 1);		
+	}
+	
+	@Test
+	public void test4() {
+		TestResult result = new TestResult();
+		TestSuite suite = new TestSuite();
+		TestCase test1 = new TestCase("test1");
+		TestCase test2 = new TestCase("test2");
+		TestCase test3 = new TestCase("test3");
+		test1.setUp(1, 1);
+		test2.setUp(1, 0);
+		suite.addTest(test1);
+		suite.addTest(test2);
+		suite.addTest(test3);
+		suite.run(result);
+		Assert.assertTrue(result.getListError().size() == 1);
+		Assert.assertTrue(result.getListFailure().size() == 1);
+		Assert.assertTrue(result.getListPassed().size() == 1);
+		
+	}
+	
+	
+
+}
